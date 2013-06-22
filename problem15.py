d195c575f445525f3f97ddb5cda8a61dda66bb81
Ping Zou <sg71.cherub@gmail.com>
2013-06-22 12:10:24 +0100

    problem 15

--- problem15.py
+++ problem15.py
@@ -1,7 +1,18 @@
-x = pow(2, 1000)
-total = 0
-while x > 0:
-    digit = x % 10
-    total += digit
-    x /= 10
-print total
+'''
+Starting in the top left corner of a 22 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+How many such routes are there through a 2020 grid?
+'''
+
+import copy
+
+n = [1]
+
+for i in xrange(0, 19):
+    last = copy.deepcopy(n)
+    n.append(0)
+    n[0] = 1
+    for j in xrange(1, len(last)):
+        n[j] = n[j-1] + last[j]
+    n[len(last)] = 2 * n[len(last)-1]
+print '==result=='
+print 2 * sum(n)
