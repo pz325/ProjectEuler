4418c0faa1ae1cc3a6fcf7ffdcb6323ed8df5ee5
Ping Zou <sg71.cherub@gmail.com>
2013-08-09 23:52:22 +0100

    problem 34

---
+++ problem34.cpp
@@ -0,0 +1,39 @@
+/*
+145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+
+Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+*/
+
+#include "util.h"
+#include <iostream>
+
+
+unsigned factorial[] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
+int main()
+{
+	unsigned N = 2540161;
+	std::vector<unsigned> factorialDigits;
+	for(unsigned i = 10; i < N; ++i)
+	{
+		std::vector<unsigned> digits = GetDigits(i);
+		unsigned sum = 0;
+		for(auto it = digits.begin(); it != digits.end(); ++it)
+		{
+			sum += factorial[*it];
+		}
+		if (sum == i)
+		{
+			factorialDigits.push_back(i);
+		}
+	}
+	unsigned sum = 0;
+	for(auto it = factorialDigits.begin(); it != factorialDigits.end(); ++it)
+	{
+		std::cout << *it << std::endl;
+		sum += *it;
+	}
+    std::cout << "Result: " << sum << std::endl;
+	return 0;
+}
