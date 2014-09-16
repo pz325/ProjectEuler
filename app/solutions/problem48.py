78c6157bc057fb69c615b3a503c870c9dab09d9a
pz325 <sg71.cherub@gmail.com>
2014-09-16 22:53:29 +0100

    problem 48

--- app/solutions/problem48.py
+++ app/solutions/problem48.py
@@ -1,24 +1,42 @@
 '''
+
+Self powers
+
 The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
 
 Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+
+
+analysis:
+
+define a product operation, return only the last 10 digits
+
+(a * b) % c = ((a % c) * (b % c)) % c
+(a + b) % c = ((a % c) + (b % c)) % c
+
 '''
 
-import traceback
-from flask import Flask
+MOD = 10000000000
+def modProduct(a, b):
+    return ((a % MOD) * (b % MOD)) % MOD
+
+def modPow(a, b):
+    p = 1
+    for i in xrange(b):
+        p = modProduct(p, a)
+    return p
+
+def modAdd(a, b):
+    return ((a % MOD) + (b % MOD)) % MOD
 
 
-def plus_last_ten(a, b):
-    pass
-
-
-def main():
-    try:
-        eval('print(a)')
-    except Exception:
-        print 'traceback\n'
-        traceback.print_exc()
+def solution():
+    result = 0
+    for i in xrange(1, 1001):
+        result = modAdd(result, modPow(i, i))
+    return result
 
 
 if __name__ == '__main__':
-    main()
+    result = solution()
+    print 'Result: ', result
