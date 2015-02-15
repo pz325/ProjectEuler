af8904a243d67960bf6de9858d342ce1a0045b5e
Ping Zou <sg71.cherub@gmail.com>
2015-02-15 17:01:07 +0000

    update problem 9

--- app/solutions/problem9.py
+++ app/solutions/problem9.py
@@ -5,26 +5,24 @@ a2 + b2 = c2
 For example, 32 + 42 = 9 + 16 = 25 = 52.
 
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+
+update: refer to Problem75
+
 Find the product abc.
 '''
 
-
 def solution():
-    found = False
-    for c in xrange(1, 1000):
-        for b in xrange(1, c):
-            for a in xrange(1, b):
-                if a * a + b * b == c * c:
-                    # print a, b, c
-                    if a + b + c == 1000:
-                        result = a * b * c
-                        found = True
-                        break
-            if found:
-                break
-        if found:
-            break
-    return result
+    N = 1000
+    max_n = int((N/2) ** 0.5)
+
+    for n in range(2, max_n):
+        for m in range(1, n):
+            a = n * n - m * m
+            b = 2 * n * m
+            c = n * n + m * m
+            l = a + b + c
+            if l == N:
+                return a * b * c
 
 if __name__ == '__main__':
     print 'Result: ', solution()
